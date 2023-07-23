8869ca8bd2ba04164a525c2adb20598263e1d5d1
Irina Saveleva <goyalion17@gmail.com>
2023-07-23 15:27:16 +0200

    refactoring

--- start/script.js
+++ start/script.js
@@ -1,5 +1,4 @@
 "use strict";
-
 const number = document.querySelector(".number");
 const reloadPage = document.querySelector(".again");
 const checkBtn = document.querySelector(".check");
@@ -8,10 +7,8 @@ const body = document.querySelector("body");
 const score = document.querySelector(".score");
 const message = document.querySelector(".message");
 const highscore = document.querySelector(".highscore");
-
 // создание секретного числа от 1 до 20
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
-
 let getScore = 20;
 let getHighscore = 0;
 
@@ -33,33 +30,22 @@ checkBtn.addEventListener("click", () => {
     }
   } else if (value !== randomNumber) {
     if (getScore > 1) {
-      if (value > randomNumber) {
-        messages("Your number is greater");
-        getScore--;
-        score.textContent = getScore;
-      } else if (value < randomNumber) {
-        messages("Your number is smaller");
-        getScore--;
-        score.textContent = getScore;
-      }
-    } else {
-      messages("You lose:(");
-      score.textContent = 0;
+      value > randomNumber
+        ? messages("Your number is greater")
+        : messages("Your number is smaller");
+      getScore--;
+      score.textContent = getScore;
     }
   }
 });
 // обновление игры
-reloadPage.addEventListener("click", () => {  
+reloadPage.addEventListener("click", () => {
   getScore = 20;
   randomNumber = Math.trunc(Math.random() * 20) + 1;
   messages("Start guessing..");
   score.textContent = getScore;
   inputNum.value = "";
-  number.textContent = "?"
+  number.textContent = "?";
   body.style.background =
     "radial-gradient(circle, rgba(35, 34, 41, 1) 51%, rgba(56, 59, 60, 1) 100%)";
-});
-// очищаем инпут при фокусе
-inputNum.addEventListener("focus", function () {
-  inputNum.value = "";
 });
